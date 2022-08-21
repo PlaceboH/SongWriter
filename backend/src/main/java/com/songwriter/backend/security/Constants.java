e2140dd306d9f8b6dedb62a36db45f636eeaafca
PlaceboH <alfaomega917@gmail.com>
2022-08-21 16:50:19 +0100

    WRITER-4 created jwt auth filter, entry point and security configuration class

--- backend/src/main/java/com/songwriter/backend/security/Constants.java
+++ backend/src/main/java/com/songwriter/backend/security/Constants.java
@@ -5,5 +5,6 @@ public class Constants {
     public static final String SECRET = "SecretKEyGenJWT";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
+    public static final String CONTENT_TYPE = "application/json";
     public static final long EXPIRATION_TIME = 1800_000; // 30min
 }
---
+++ backend/src/main/java/com/songwriter/backend/security/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.songwriter.backend.security;
+
+import com.songwriter.backend.Services.CustomUserDetailsService;
+import org.hibernate.criterion.Example;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        proxyTargetClass = true
+)
+public class SecurityConfig {
+    @Autowired
+    private JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                        .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+                        .sessionManagement()
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                        .authorizeRequests()
+                        .antMatchers(Constants.SIGN_UP_URL).permitAll()
+                        .anyRequest().authenticated();
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    protected AuthenticationManager authenticationManager(AuthenticationManagerBuilder builder) throws Exception {
+        return (AuthenticationManager) builder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JWTAuthenticationFilter jwtAuthenticationFilter(){
+        return new JWTAuthenticationFilter();
+    }
+
+}
