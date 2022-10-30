0dd26de57dcea67f9fbc78c0be23c719ae03fb0d
PlaceboH <alfaomega917@gmail.com>
2022-10-30 16:29:16 +0100

    init angular app and start home page

---
+++ frontend/src/app/walcome-page/welcome-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    imports: [],
+    selector: 'welcome-page',
+    templateUrl: './welcome-page.component.html',
+    styleUrls: ['./welcome-page.component.scss']
+  })
+  export class WelcomePageComponent {
+  
+  }
