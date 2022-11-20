c12c7053e395c7ea676cffe4fcb47097f3a4ddc4
PlaceboH <alfaomega917@gmail.com>
2022-11-20 13:37:39 +0100

    WRITER-I remove unused dependancy

--- backend/src/main/java/com/songwriter/backend/security/Constants.java
+++ backend/src/main/java/com/songwriter/backend/security/Constants.java
@@ -2,7 +2,7 @@ package com.songwriter.backend.security;
 
 public class Constants {
     public static final String SIGN_UP_URL = "/api/auth/**";
-    public static final String SECRET = "SecretKEyGenJWT";
+    public static final String SECRET = "SecretKeyGenJWT";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String CONTENT_TYPE = "application/json";
